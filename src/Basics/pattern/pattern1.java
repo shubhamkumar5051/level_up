5b5f91642467c35d1caa2acd9a8808ccea9fe1f1
shubham <shubhamkr5051@gmail.com>
2026-02-08 10:39:34 +0530

    added files

---
+++ src/Basics/pattern/pattern1.java
@@ -0,0 +1,12 @@
+package Basics.pattern;
+
+import java.util.*;
+import java.util.Scanner;
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
+        sc.close();
+    }
+}
